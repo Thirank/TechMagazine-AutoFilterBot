08f6e5a6761f5c1db24de9433867f5716d377844
Thirank <116568300+Thirank@users.noreply.github.com>
2023-01-14 19:54:53 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -23,7 +23,7 @@ async def start(client, message):
     if message.chat.type in [enums.ChatType.GROUP, enums.ChatType.SUPERGROUP]:
         buttons = [
             [
-                InlineKeyboardButton('🤖 Updates', url='https://t.me/m_o_c_Upate')
+                InlineKeyboardButton('🤖 Updates', url='https://t.me/Moc_Hd')
             ],
             [
                 InlineKeyboardButton('ℹ️ Help', url=f"https://t.me/{temp.U_NAME}?start=help"),
@@ -42,10 +42,10 @@ async def start(client, message):
         await client.send_message(LOG_CHANNEL, script.LOG_TEXT_P.format(message.from_user.id, message.from_user.mention))
     if len(message.command) != 2:
         buttons = [[
-            InlineKeyboardButton('➕  Discussion  ➕', url='https://t.me/mightymoviesgroup')
+            InlineKeyboardButton('➕  ⚡Discussion⚡  ➕', url='https://t.me/mcmoviegroup')
             ],[
             InlineKeyboardButton('🔍 Search', switch_inline_query_current_chat=''),
-            InlineKeyboardButton('🤖 Updates', url='https://t.me/m_o_c_Upate')
+            InlineKeyboardButton('🤖 Updates', url='https://t.me/Moc_Hd')
             ],[
             InlineKeyboardButton('ℹ️ Help', callback_data='help'),
             InlineKeyboardButton('😊 About', callback_data='about')
@@ -88,10 +88,10 @@ async def start(client, message):
         return
     if len(message.command) == 2 and message.command[1] in ["subscribe", "error", "okay", "help"]:
         buttons = [[
-            InlineKeyboardButton('➕ Discussion  ➕', url='https://t.me/mightymoviesgroup')
+            InlineKeyboardButton('➕ ⚡Discussion⚡  ➕', url='https://t.me/mcmoviegroup')
             ],[
             InlineKeyboardButton('🔍 Search', switch_inline_query_current_chat=''),
-            InlineKeyboardButton('🤖 Updates', url='https://t.me/m_o_c_Upate')
+            InlineKeyboardButton('🤖 Updates', url='https://t.me/Moc_Hd')
             ],[
             InlineKeyboardButton('ℹ️ Help', callback_data='help'),
             InlineKeyboardButton('😊 About', callback_data='about')
